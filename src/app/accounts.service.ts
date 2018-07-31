0fdf43048a9328c90c33c16f21c243b6466df0da
Amit Buhnik <P0020755@ness-tech.co.il>
2018-07-31 12:15:20 +0300

    cross component communication throgh service

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { LoggingService } from './logging.service';
 
 @Injectable({
   providedIn: 'root'
@@ -19,20 +21,22 @@ export class AccountsService {
     }
   ];
 
-  constructor() { }
+  statusUpdated = new EventEmitter<string>();
+
+  constructor(private loggingService: LoggingService) {     
+  }
 
   getAccounts(): { name: string, status: string } [] {
     return this.accounts;
   }
 
   addAccount(name: string, status: string) {
-    this.accounts.push({
-      name: name,
-      status: status
-    });
+    this.accounts.push({ name: name, status: status });
+    this.loggingService.logStatusChange(status);
   }
 
   updateStatus(id: number, status: string) {
     this.accounts[id].status = status;
+    this.loggingService.logStatusChange(status);
   }
 }
